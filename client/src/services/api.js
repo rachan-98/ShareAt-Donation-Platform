07d863a35e5dc2bb98cf9012b9f6adb7863ccdce
Rachan Kumar Gantha <rachan98gantha@gmail.com>
2026-06-10 00:25:55 +0530

    Add Vercel environment variable support

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://shareat-backend.onrender.com/api",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "http://localhost:5000/api",
 });
 
 API.interceptors.request.use((config) => {
